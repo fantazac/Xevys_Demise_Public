4fb75cc9bf2f3fd67e423e156073a72ad53f3e45
Vincent Dumont <vdumont@monancai.com>
2016-09-28 10:54:40 -0400

    Character can now attack left and right with K

--- Assets/Scripts/Actors/Attack/ActorBasicAttack.cs
+++ Assets/Scripts/Actors/Attack/ActorBasicAttack.cs
@@ -4,18 +4,32 @@ using System.Collections;
 public class ActorBasicAttack : MonoBehaviour
 {
     private InputManager _inputManager;
-    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+    private int _count = 30;
 
 	void Start ()
 	{
         _inputManager = GetComponent<InputManager>();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+	    _spriteRenderer = GameObject.Find("CharacterBasicAttackBoxSprite").GetComponent<SpriteRenderer>();
 
 	    _inputManager.OnBasicAttack += OnBasicAttack;
 	}
 
+    void Update()
+    {
+        _count++;
+        if (_count >= 30)
+        {
+            _spriteRenderer.enabled = false;
+        }
+    }
+
     void OnBasicAttack()
     {
-        
+        if (_count >= 30)
+        {
+            _spriteRenderer.enabled = true;
+            _count = 0;
+        }          
     }
 }
