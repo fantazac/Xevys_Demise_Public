1978c3117f6f0ced9e866577f0454ae20ed7f492
Vincent Dumont <vdumont@monancai.com>
2016-09-28 08:30:32 -0400

    Removed InputHandler, added ActorBasicAttack

---
+++ Assets/Scripts/Actors/ActorBasicAttack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorBasicAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
