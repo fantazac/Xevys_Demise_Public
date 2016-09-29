ad0a70b5537282ca4c6ad04c8230606efd382bd3
Alexandre <alex>
2016-09-29 13:56:08 -0400

    1-6 trigger created & successfully removes locked door in 1-5

---
+++ Assets/Scripts/Environment/OpenDoorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoorTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _door;
+
+    [SerializeField]
+    private GameObject[] _wallsToActivate;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (_door != null && collider.gameObject.tag == "Axe")
+        {
+            foreach (GameObject wall in _wallsToActivate)
+                wall.GetComponent<BoxCollider2D>().enabled = true;
+
+            Destroy(_door);
+            Destroy(gameObject);
+        }
+    }
+}
