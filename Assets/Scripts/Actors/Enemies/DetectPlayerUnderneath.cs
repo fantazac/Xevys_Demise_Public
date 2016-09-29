d8c09df9f545e284109156bd8b1693ad6b5d5a8d
Niko <corseplusplus@gmail.com>
2016-09-29 15:50:30 -0400

    Added a personnal sandbox and a bat.

---
+++ Assets/Scripts/Actors/Enemies/DetectPlayerUnderneath.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectPlayerUnderneath : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 2);
+        }
+
+    }
+}
