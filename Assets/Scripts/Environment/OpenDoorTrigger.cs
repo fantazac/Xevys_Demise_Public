e50ca2964e19f64bf61e71e2416b590d3aed5c36
Philippe Boutin <philboutin@hotmail.com>
2016-09-29 14:29:14 -0400

    Added sound on purple switch activated

--- Assets/Scripts/Environment/OpenDoorTrigger.cs
+++ Assets/Scripts/Environment/OpenDoorTrigger.cs
@@ -10,12 +10,17 @@ public class OpenDoorTrigger : MonoBehaviour
     [SerializeField]
     private GameObject[] _wallsToActivate;
 
+    [SerializeField]
+    private AudioSource _audio;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (_door != null && collider.gameObject.tag == "Axe")
         {
             foreach (GameObject wall in _wallsToActivate)
                 wall.GetComponent<BoxCollider2D>().enabled = true;
+            
+            _audio.Play();
 
             Destroy(_door);
             Destroy(gameObject);
