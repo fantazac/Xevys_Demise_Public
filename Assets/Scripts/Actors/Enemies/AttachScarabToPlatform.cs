469ccf9dd51e44139c750c37e07d6691d7d06e75
Alexandre <alex>
2016-09-29 15:43:27 -0400

    Scarab movement initialised

--- Assets/Scripts/Actors/Enemies/AttachScarabToPlatform.cs
+++ Assets/Scripts/Actors/Enemies/AttachScarabToPlatform.cs
@@ -10,40 +10,46 @@ public class AttachScarabToPlatform : MonoBehaviour
     private Vector3 _wallPosition;
     private Vector3 _wallScale;
 
-    private int currentPoint = 0;
+    private int _currentPoint = 0;
 
     private bool _rotate = false;
 
-    private Vector2 _target;
-    private Vector2[] _points;
+    private Vector3 _target;
+    private Vector3[] _points;
 
     private void Start()
     {
-        if(_attachedWall != null)
+        if (_attachedWall != null)
         {
-            _points = new Vector2[4];
+            _points = new Vector3[4];
 
             _wallPosition = _attachedWall.GetComponent<Transform>().position;
             _wallScale = _attachedWall.GetComponent<Transform>().localScale;
 
-            _points[0] = new Vector2(_wallPosition.x - _wallScale.x / 2, _wallPosition.y - _wallScale.y);
-            _points[1] = new Vector2(_wallPosition.x + _wallScale.x / 2, _wallPosition.y - _wallScale.y);
-            _points[2] = new Vector2(_wallPosition.x + _wallScale.x / 2, _wallPosition.y + _wallScale.y);
-            _points[3] = new Vector2(_wallPosition.x - _wallScale.x / 2, _wallPosition.y + _wallScale.y);
+            _points[0] = new Vector2(_wallPosition.x - _wallScale.x / 2 - transform.localScale.x / 2, _wallPosition.y - _wallScale.y / 2 - transform.localScale.y / 2);
+            _points[1] = new Vector2(_wallPosition.x + _wallScale.x / 2 + transform.localScale.x / 2, _wallPosition.y - _wallScale.y / 2 - transform.localScale.y / 2);
+            _points[2] = new Vector2(_wallPosition.x + _wallScale.x / 2 + transform.localScale.x / 2, _wallPosition.y + _wallScale.y / 2 + transform.localScale.y / 2);
+            _points[3] = new Vector2(_wallPosition.x - _wallScale.x / 2 - transform.localScale.x / 2, _wallPosition.y + _wallScale.y / 2 + transform.localScale.y / 2);
 
+            _target = _points[0];
+            transform.position = _points[0];
             FindTarget();
         }
-        
+
     }
 
     private void Update()
     {
-        
+        if (_target != transform.position)
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), _target, 1 * Time.deltaTime);
+        else
+            FindTarget();
     }
 
     private void FindTarget()
     {
-        //if()
+        if (_target == transform.position)
+            _target = _points[(++_currentPoint) % _points.Length];
     }
 
 }
