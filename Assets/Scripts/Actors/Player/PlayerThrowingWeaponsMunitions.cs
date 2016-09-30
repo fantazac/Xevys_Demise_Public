fbfac8ebb7f6ff4da5b1a0bcec7d72d276e51842
Platou <william.cote@bell.net>
2016-09-30 14:06:37 -0400

    Working on picking Items

--- Assets/Scripts/Actors/Player/PlayerThrowingWeaponsMunitions.cs
+++ Assets/Scripts/Actors/Player/PlayerThrowingWeaponsMunitions.cs
@@ -1,18 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerThrowingWeaponsMunitions : MonoBehaviour
 {
 
-    private PickThrowingWeaponsMunitions _munitionsPicker;
+    private IList<PickThrowingWeaponsMunitions> _munitionsPicker;
 
     private int _axeMunition = 0;
     public int AxeMunition { get { return _axeMunition; } set { _axeMunition = value; } }
 
+    private int _knifeMunition = 0;
+    public int KnifeMunition { get { return _knifeMunition; } set { _knifeMunition = value; } }
+
     private void Start()
     {
-        _munitionsPicker = GameObject.Find("BaseAxeItem").GetComponent<PickThrowingWeaponsMunitions>();
-        _munitionsPicker.OnAxePicked += OnAxePicked;
+        _munitionsPicker.Add(GameObject.Find("BaseAxeItem").GetComponent<PickThrowingWeaponsMunitions>());
+        _munitionsPicker.Add(GameObject.Find("BaseKnifeItem").GetComponent<PickThrowingWeaponsMunitions>());
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("KnifePickableItem"))
+        {
+            
+        }
+
+        foreach (PickThrowingWeaponsMunitions munitionsItem in _munitionsPicker)
+        {
+            
+        }
     }
 
     private void OnAxePicked(int axeAmount)
@@ -20,5 +33,12 @@ public class PlayerThrowingWeaponsMunitions : MonoBehaviour
         _axeMunition += axeAmount;
         _munitionsPicker.OnAxePicked -= OnAxePicked;
         Destroy(GameObject.Find("BaseAxeItem"));
+    }
+
+    private void OnKnifePicked(int axeAmount)
+    {
+        _axeMunition += axeAmount;
+        _munitionsPicker.OnAxePicked -= OnAxePicked;
+        Destroy(GameObject.Find("BaseKnifeItem"));
     }
 }
--- Assets/Scripts/Item/PickThrowingWeaponsMunitions.cs
+++ Assets/Scripts/Item/PickThrowingWeaponsMunitions.cs
@@ -11,25 +11,23 @@ public class PickThrowingWeaponsMunitions : MonoBehaviour
 
     [SerializeField]
     private const int AXE_AMOUNT_ON_PICKUP = 5;
-
     private const int BASE_AXE_AMOUNT_ON_PICKUP = 10;
+    [SerializeField]
+    private const int KNIFE_AMOUNT_ON_PICKUP = 5;
+    private const int BASE_KNIFE_AMOUNT_ON_PICKUP = 10;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Player")
         {
-            if (gameObject.tag == "Knife")
-            {
-                OnKnifePicked(1);
-            }
+            if (gameObject.tag == "KnifePickableItem")
+                OnKnifePicked(KNIFE_AMOUNT_ON_PICKUP);
+            else if (gameObject.tag == "BaseKnifeItem")
+                OnKnifePicked(BASE_KNIFE_AMOUNT_ON_PICKUP);
             else if (gameObject.tag == "AxePickableItem")
-            {
                 OnAxePicked(AXE_AMOUNT_ON_PICKUP);
-            }
             else if (gameObject.tag == "BaseAxeItem")
-            {
                 OnAxePicked(BASE_AXE_AMOUNT_ON_PICKUP);
-            }
         }
     }
 }
