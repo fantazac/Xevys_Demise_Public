c91ca56fd800a90faa5bce92bd888c86aa0b0b27
Alexandre <alex>
2016-09-30 16:20:40 -0400

    Added 1 scarab in 1-1 & changed ItemHover script

---
+++ Assets/Scripts/Environment/ItemHover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemHover : MonoBehaviour
+{
+    private const float SPEED = 0.11f;
+    private const float WAVE_LENGTH = 0.08f;
+
+    private float _sinCount = 0;
+
+
+    private float _initialYPosition;
+
+    private void Start()
+    {
+        _initialYPosition = transform.position.y;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector2(transform.position.x, _initialYPosition + WAVE_LENGTH * Mathf.Sin(_sinCount));
+        _sinCount += SPEED;
+    }
+}
