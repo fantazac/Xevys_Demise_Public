4f4eb8018b617e151bbd4b7c42f36c99f2c096c4
Philippe Boutin <philboutin@hotmail.com>
2016-09-30 12:44:38 -0400

    Added scripts to get damage when touching scarabs

---
+++ Assets/Scripts/Actors/Player/PlayerDamageManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamageManager : MonoBehaviour
+{
+    public delegate void OnDamageHandler(int damage);
+    public event OnDamageHandler OnDamage;
+    
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+            OnDamage(100);
+    }
+}
