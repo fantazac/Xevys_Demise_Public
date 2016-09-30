4c18e6b66ebb4ec910cd8e0c9cb4505f46a05eb7
Alexandre <alex>
2016-09-29 22:53:21 -0400

    Rotate regular scarabs

--- Assets/Scripts/Actors/Enemies/AttachScarabToPlatform.cs
+++ Assets/Scripts/Actors/Enemies/AttachScarabToPlatform.cs
@@ -7,10 +7,15 @@ public class AttachScarabToPlatform : MonoBehaviour
     [SerializeField]
     private GameObject _attachedWall;
 
+    private const float MAX_ROTATION = 90;
+
     private Vector3 _wallPosition;
     private Vector3 _wallScale;
 
     private int _currentPoint;
+
+    private bool _rotate = false;
+    private float _rotateCount = 0;
 
     private Vector3 _target;
     private Vector3[] _points;
@@ -29,7 +34,7 @@ public class AttachScarabToPlatform : MonoBehaviour
             _points[2] = new Vector2(_wallPosition.x + _wallScale.x / 2 + transform.localScale.x / 2, _wallPosition.y + _wallScale.y / 2 + transform.localScale.y / 2);
             _points[3] = new Vector2(_wallPosition.x - _wallScale.x / 2 - transform.localScale.x / 2, _wallPosition.y + _wallScale.y / 2 + transform.localScale.y / 2);
 
-            _currentPoint = Random.Range(0, _points.Length-1);
+            _currentPoint = Random.Range(0, _points.Length - 1);
 
             _target = _points[_currentPoint];
             transform.position = _points[_currentPoint];
@@ -41,9 +46,28 @@ public class AttachScarabToPlatform : MonoBehaviour
     private void Update()
     {
         if (_target != transform.position)
+        {
             transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), _target, 1 * Time.deltaTime);
+            if (Vector3.Distance(_target, transform.position) <= transform.localScale.x / 2)
+                _rotate = true;
+        }
         else
             FindTarget();
+
+        if (_rotate)
+            if (_rotateCount < MAX_ROTATION)
+            {
+                _rotateCount += 2;
+                transform.Rotate(Vector3.forward * 2);
+            }
+
+            else
+            {
+                _rotate = false;
+                _rotateCount = 0;
+            }
+
+
     }
 
     private void FindTarget()
--- Assets/Scripts/Actors/Player/PlayerMovement.cs
+++ Assets/Scripts/Actors/Player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement: MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     private InputManager _inputManager;
