b714a7c22042042b586c67a1f99633a8adf77d43
William <william.cote@bell.net>
2016-09-30 15:42:37 -0400

    Adding hover pad prefab + sandbox update

---
+++ Assets/Scripts/Environment/ItemHoverPad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemHoverPad : MonoBehaviour
+{
+    [SerializeField]
+    private float _hoverForce = 8f;
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.tag == "BaseKnifeItem")
+        {
+            collider.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * _hoverForce);
+        }
+    }
+}
