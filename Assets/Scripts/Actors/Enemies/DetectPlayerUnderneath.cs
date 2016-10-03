754057aabd87a20713531890a84e862ade81bdc4
Niko <corseplusplus@gmail.com>
2016-10-03 08:36:30 -0400

    Fixed bat and modified it.

--- Assets/Scripts/Actors/Enemies/DetectPlayerUnderneath.cs
+++ Assets/Scripts/Actors/Enemies/DetectPlayerUnderneath.cs
@@ -5,12 +5,6 @@ public class DetectPlayerUnderneath : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(!GetComponentInParent<MoveBatInDelimitedArea>().IsPlayerUnderneath)
-        {
-            if (collider.gameObject.tag == "Player" && collider is BoxCollider2D)
-            {
-                GetComponentInParent<MoveBatInDelimitedArea>().IsPlayerUnderneath = true;
-            }
-        }
+
     }
 }
