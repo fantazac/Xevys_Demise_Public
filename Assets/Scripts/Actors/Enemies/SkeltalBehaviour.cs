b7f9b49f3516c8d4dd947aebde237998d1b17fcb
Niko <corseplusplus@gmail.com>
2016-10-03 20:45:31 -0400

    Creation of Skeltal AI and division of subclasses into components. The Jumpy Skeltal has a buggy movement, however.

---
+++ Assets/Scripts/Actors/Enemies/SkeltalBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SkeltalBehaviour : MonoBehaviour {
+
+    protected const float ATTACK_TIME = 3;
+    protected const float SPEED = 5;
+    [SerializeField]
+    protected float _leftLimit;
+    [SerializeField]
+    protected float _rightLimit;
+    protected float _initialHeight;
+    protected bool _isAttacking;
+    protected float _attackTimeLeft;
+    //In the upcoming development, it would be wise to implement this variable into a component.
+    protected bool _isFacingRight;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _initialHeight = transform.position.y;
+        _isFacingRight = false;
+        _isAttacking = false;
+        _attackTimeLeft = ATTACK_TIME;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!_isAttacking)
+        {
+            _isAttacking = MoveSkeltal();
+        }
+        else
+        {
+            _attackTimeLeft -= Time.fixedDeltaTime;
+            if (_attackTimeLeft < 2 && _attackTimeLeft > 1)
+            {
+                //Attack
+            }
+            else if (_attackTimeLeft <= 0)
+            {
+                _attackTimeLeft = ATTACK_TIME;
+                _isAttacking = false;
+                Flip();
+            }
+        }
+    }
+
+    protected abstract bool MoveSkeltal();
+
+    //In upcoming development, it would be wise to implement this method into a Component.
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
+    }
+}
