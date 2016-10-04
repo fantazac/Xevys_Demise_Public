b9eb18a8e2a0e3f9ec39c9d1cbe927a931b804a4
Vincent Dumont <dumontv@outlook.com>
2016-10-03 22:37:17 -0400

    Health bar now changes color according to health

--- Assets/Scripts/UI/ScaleHealthBar.cs
+++ Assets/Scripts/UI/ScaleHealthBar.cs
@@ -1,50 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScaleHealthBar : MonoBehaviour
 {
     [SerializeField]
     public const int MAX_HEALTH = 1000;
 
-    private float _initialTriangleX;
     private float _initialRectangleX;
-    private Transform _healthTriangle;
-    private Transform _healthRectangle;
+    private Transform _healthBar;
+    private Image _healthBarImage;
     private Health _health;
 
 	private void Start ()
 	{
 	    _health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
-	    _healthTriangle = GameObject.Find("HealthBarTriangle").transform;
-        _healthRectangle = GameObject.Find("HealthBarRectangle").transform;
-	    _initialRectangleX = _healthTriangle.localScale.x;
-	    _initialTriangleX = _healthRectangle.localScale.x;
+        _healthBar = GameObject.Find("HealthBar").GetComponent<Transform>();
+	    _healthBarImage = GameObject.Find("HealthBar").GetComponent<Image>();
+	    _initialRectangleX = _healthBar.localScale.x;
+	    _healthBarImage.color = new Color(0, 1, 0, 1);
 
-	    _health.OnHealthChanged += OnHealthChanged;
-	}
-	
-	private void Update ()
-	{
-	    
+        _health.OnHealthChanged += OnHealthChanged;
 	}
 
     private void OnHealthChanged(int hitPoints)
     {
-        // Si la vie est à 90% du max ou plus, on travaille sur le triangle
-        if (_healthTriangle.localScale.x > 0)
-        {         
-            _healthTriangle.localScale = new Vector2(_healthTriangle.localScale.x - (100 - ((_health.HealthPoint - hitPoints) * 100) / MAX_HEALTH) * _initialTriangleX / 100, 
-                _healthTriangle.localScale.y);          
-        }
-
-        if (_healthTriangle.localScale.x <= 0 && _healthRectangle.localScale.x > 0)
+        if (_healthBar.localScale.x > 0)
         {
-            _healthTriangle.localScale = Vector2.zero;
-            _healthRectangle.localScale = new Vector2(_healthRectangle.localScale.x - (100 - ((_health.HealthPoint - hitPoints) * 100) / MAX_HEALTH) *  _initialRectangleX / 100,
-                _healthRectangle.localScale.y);
-            _healthRectangle.localPosition = new Vector2();
-        }
+            _healthBar.localScale = new Vector3(_healthBar.localScale.x - (100 - ((_health.HealthPoint - hitPoints) * 100) / MAX_HEALTH) *  _initialRectangleX / 100,
+                _healthBar.localScale.y, _healthBar.localScale.z);
 
+            if (_health.HealthPoint - hitPoints >= 50f*MAX_HEALTH/100f)
+            {
+                _healthBarImage.color = new Color((1 - ((_health.HealthPoint - hitPoints)*100/MAX_HEALTH * 1 / 100)) * 10, 1, 0, 1);
+            }
+        }    
         _health.HealthPoint -= hitPoints;
     }
 }
