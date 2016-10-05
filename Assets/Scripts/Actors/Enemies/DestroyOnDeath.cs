cbc66834cff6fbaa6b68e3f426942201077c016f
Alexandre <fantazamec1@hotmail.com>
2016-10-05 08:32:11 -0400

    Added tag to scarab and added DestroyOnDeath scripts to enemies

---
+++ Assets/Scripts/Actors/Enemies/DestroyOnDeath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnDeath : MonoBehaviour
+{
+
+    private Health _health;
+
+    private void Start()
+    {
+        _health = GetComponent<Health>();
+    }
+
+    private void Update()
+    {
+        if (_health.HealthPoint <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
