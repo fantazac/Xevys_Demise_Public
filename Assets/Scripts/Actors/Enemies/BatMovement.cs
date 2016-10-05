6e443897b76dc6078ee4b5b5aada9a4dc8a7412b
Alexandre <fantazamec1@hotmail.com>
2016-10-05 09:44:54 -0400

    Player jump bug on edges fixed

--- Assets/Scripts/Actors/Enemies/BatMovement.cs
+++ Assets/Scripts/Actors/Enemies/BatMovement.cs
@@ -22,7 +22,6 @@ public class BatMovement : MonoBehaviour
 
     private bool _isInPosition = true;
     private bool _startCooldown = false;
-    private bool _goingUp = false;
     private bool _goingDown = false;
     private int _cooldownCount = 0;
 
@@ -39,22 +38,12 @@ public class BatMovement : MonoBehaviour
         _maxX = _initialPosition.x + _rightDistance;
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.gameObject.tag == "Player" && !_goingUp)
-        {
-            _startCooldown = true;
-            _goingUp = true;
-        }
-    }*/
-
     private void Update()
     {
         if (transform.position.y < _lowestY)
         {
             transform.position = new Vector3(transform.position.x, _lowestY, transform.position.z);
             _startCooldown = true;
-            _goingUp = true;
         }
 
         _playerDetectionHitbox.transform.position = new Vector3(transform.position.x, _playerDetectionHitbox.transform.position.y, transform.position.z);
@@ -70,7 +59,6 @@ public class BatMovement : MonoBehaviour
             if (transform.position.y == _target.y)
             {
                 _isInPosition = true;
-                _goingUp = false;
             }
         }
         else if (!_startCooldown && _goingDown)
