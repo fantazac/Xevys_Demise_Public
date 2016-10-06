6bdc526c0ff5493597d11d64ee06ad3da3a3426e
Vincent Dumont <vdumont@monancai.com>
2016-10-06 10:01:28 -0400

    UI now shows current ammo on throwable weapons

---
+++ Assets/Scripts/UI/ShowEquippedWeapons.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowEquippedWeapons : MonoBehaviour
+{
+    private Text _knifeText;
+    private Text _axeText;
+    private SpriteRenderer _knifeSpriteRenderer;
+    private SpriteRenderer _axeSpriteRenderer;
+
+    private void Start()
+    {
+        _knifeText = GameObject.Find("KnifeAmmo").GetComponent<Text>();
+        _axeText = GameObject.Find("AxeAmmo").GetComponent<Text>();
+        _knifeSpriteRenderer = GameObject.Find("WeaponSelectKnife").GetComponent<SpriteRenderer>();
+        _axeSpriteRenderer = GameObject.Find("WeaponSelectAxe").GetComponent<SpriteRenderer>();
+
+        _knifeText.enabled = false;
+        _axeText.enabled = false;
+        _knifeSpriteRenderer.enabled = false;
+        _axeSpriteRenderer.enabled = false;
+    }
+
+    public void OnKnifeAmmoChanged(int total)
+    {
+        if (!_knifeSpriteRenderer.enabled)
+        {
+            _knifeSpriteRenderer.enabled = true;
+            _knifeText.enabled = true;
+        }
+        _knifeText.text = total.ToString();
+    }
+
+    public void OnAxeAmmoChanged(int total)
+    {
+        if (!_axeSpriteRenderer.enabled)
+        {
+            _axeSpriteRenderer.enabled = true;
+            _axeText.enabled = true;
+        }
+        _axeText.text = total.ToString();
+    }
+
+}
