3a9c22ec792b221edd3b5d7302c7e3f7d8ee7c7a
Platou <william.cote@bell.net>
2016-10-06 10:04:59 -0400

    Axe and knife are destroy on enemy hit

---
+++ Assets/Scripts/Weapon/ThrowingWeaponHitEnemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowingWeaponHitEnemy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Scarab" ||
+            collider.gameObject.tag == "Bat" ||
+            collider.gameObject.tag == "Skeltal")
+        {
+            GetComponent<DestroyWeapon>().DestroyNow = true;
+        }
+    }
+}
