d6efe9b810b96a350f1afa19e8854baae8d7b23d
Niko <corseplusplus@gmail.com>
2016-10-07 09:45:00 -0400

    Refined Bouncy Skeltal for height stopping variation and corrected the Walker Skeltal prefab.

--- Assets/Scripts/Actors/Enemies/MoveBouncySkeltal.cs
+++ Assets/Scripts/Actors/Enemies/MoveBouncySkeltal.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class MoveBouncySkeltal : SkeltalBehaviour
 {
+    [SerializeField]
+    private float _rightHeightLimit;
+    [SerializeField]
+    private float _leftHeightLimit;
+
     private float _newHeight;
     private const float BOUNCY_SPEED = 0.1f;
+    private float _bounceApex;
 
     [SerializeField]
     private float _bounceHeight;
@@ -16,13 +22,24 @@ public class MoveBouncySkeltal : SkeltalBehaviour
         {
             _bounceHeight = 1;
         }
+        _bounceHeight = Mathf.Abs(_bounceHeight);
+        _bounceApex = (_initialPosition.x - _leftLimit + _initialPosition.x + _rightLimit) / 2;
     }
 
     protected override bool UpdateSkeltal()
     {
-        _newHeight = -((transform.position.x - (_initialPosition.x - _leftLimit)) * (transform.position.x - (_initialPosition.x + _rightLimit)))/_bounceHeight + _initialPosition.y;
+        _newHeight = -((transform.position.x + (_isFacingRight ? BOUNCY_SPEED : -BOUNCY_SPEED) - (_initialPosition.x - _leftLimit)) 
+                    * (transform.position.x + (_isFacingRight ? BOUNCY_SPEED : -BOUNCY_SPEED) - (_initialPosition.x + _rightLimit)))/_bounceHeight + _initialPosition.y;
         transform.position = new Vector2(transform.position.x + (_isFacingRight ? BOUNCY_SPEED : -BOUNCY_SPEED), _newHeight);
 
+        if (_isFacingRight && transform.position.x > _bounceApex && transform.position.y <= _initialPosition.y + _rightHeightLimit)
+        {
+            return true;
+        }
+        if (!_isFacingRight && transform.position.x < _bounceApex && transform.position.y <= _initialPosition.y + _leftHeightLimit)
+        {
+            return true;
+        }
         if ((_isFacingRight && transform.position.x >= _initialPosition.x + _rightLimit) || (!_isFacingRight && transform.position.x <= _initialPosition.x - _leftLimit))
         {
             return true;
