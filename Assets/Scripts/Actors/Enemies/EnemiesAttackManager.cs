d371ab6a7fa51dacb362fe2f7c2eb32f0b9762c2
Platou <william.cote@bell.net>
2016-10-07 10:38:06 -0400

    Major update on combat structure

---
+++ Assets/Scripts/Actors/Enemies/EnemiesAttackManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemiesAttackManager : MonoBehaviour
+{
+    [SerializeField]
+    private int _baseDamage = 100;
+    private int _baseDamageTimer;
+
+    private int _damageTimer = 0;
+
+    private void Start()
+    {
+        _baseDamageTimer = (int)GameObject.Find("Character").GetComponent<InvincibilityAfterBeingHit>().InvincibilityTime;
+    }
+
+    private void Update()
+    {
+        _damageTimer--;
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player" &&
+            !collider.GetComponent<InvincibilityAfterBeingHit>().IsFlickering &&
+            _damageTimer <= 0)
+        {
+            collider.GetComponent<Health>().Hit(_baseDamage);
+            collider.GetComponent<KnockbackOnDamageTaken>().KnockbackPlayer(transform.position);
+            collider.GetComponent<InvincibilityAfterBeingHit>().StartFlicker();
+
+            _damageTimer = _baseDamageTimer;
+        }
+    }
+}
