5feeff50434a9011251621ca80038f4bc4bb1246
Platou <william.cote@bell.net>
2016-10-19 10:53:05 -0400

    No more invincibility frames for the enemies

--- Assets/Scripts/Actors/Player/PlayerBasicAttackManager.cs
+++ Assets/Scripts/Actors/Player/PlayerBasicAttackManager.cs
@@ -6,30 +6,19 @@ public class PlayerBasicAttackManager : MonoBehaviour
 {
     [SerializeField]
     private int _baseDamage = 100;
-    private int _baseDamageTimer;
-
-    private int _damageTimer = 0;
 
     private string[] _enemiesTags;
 
     private void Start()
     {
         _enemiesTags = new string[] { "Scarab", "Bat", "Skeltal", "Behemoth", "Phoenix", "Spike" };
-        _baseDamageTimer = 50;
-    }
-
-
-    private void Update()
-    {
-        _damageTimer--;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (_enemiesTags.Contains(collider.gameObject.tag) && _damageTimer <= 0)
+        if (_enemiesTags.Contains(collider.gameObject.tag))
         {
             collider.GetComponent<Health>().Hit(_baseDamage);
-            _damageTimer = _baseDamageTimer;
         }
     }
 }
