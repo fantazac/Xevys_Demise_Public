59af817b7eb96e3bc54805336685e75c28cc6f57
Vincent Dumont <vdumont@monancai.com>
2016-10-20 13:23:47 -0400

    Character now has attack animation

--- Assets/Scripts/Actors/Attack/ActorBasicAttack.cs
+++ Assets/Scripts/Actors/Attack/ActorBasicAttack.cs
@@ -9,31 +9,38 @@ public class ActorBasicAttack : MonoBehaviour
     private InputManager _inputManager;
     private GameObject _attackHitBox;
     private AudioSource[] _audioSources;
+    private Animator _anim;
+
+    private bool _isAttacking = false;
     private int _count;
 
-    void Start()
+    private void Start()
     {
         _inputManager = GetComponent<InputManager>();
         _attackHitBox = GameObject.Find("CharacterBasicAttackBox");
+        _anim = GameObject.Find("CharacterSprite").GetComponent<Animator>();
         _audioSources = GetComponents<AudioSource>();
         _count = ATTACK_SPEED;
 
         _inputManager.OnBasicAttack += OnBasicAttack;
     }
 
-    void Update()
+    private void Update()
     {
+        _anim.SetBool("IsAttacking", _isAttacking);
         _count++;
         if (_count >= ATTACK_SPEED / 2)
         {
             _attackHitBox.GetComponent<BoxCollider2D>().enabled = false;
+            _isAttacking = false;
         }
     }
 
-    void OnBasicAttack()
+    private void OnBasicAttack()
     {
-        if (_count >= ATTACK_SPEED)
+        if (_count >= ATTACK_SPEED && GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().velocity.y >= 0)
         {
+            _isAttacking = true;
             _attackHitBox.GetComponent<BoxCollider2D>().enabled = true;
             _count = 0;
 
