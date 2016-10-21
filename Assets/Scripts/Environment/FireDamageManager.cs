1ac95409e5d67cca3fdaf0300f69345dfb28d2ec
Philippe Boutin <philboutin@hotmail.com>
2016-10-21 09:36:14 -0400

    Fire environment element added

---
+++ Assets/Scripts/Environment/FireDamageManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireDamageManager : MonoBehaviour
+{
+    [SerializeField]
+    private int _baseDamage = 100;
+    private int _baseDamageTimer;
+
+    private int _damageTimer = 0;
+
+    private void Start()
+    {
+        _baseDamageTimer = (int)GameObject.Find("Character").GetComponent<InvincibilityAfterBeingHit>().InvincibilityTime;
+    }
+
+    private void Update()
+    {
+        _damageTimer--;
+    }
+
+    private void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player" &&
+            !coll.gameObject.GetComponent<InvincibilityAfterBeingHit>().IsFlickering &&
+            _damageTimer <= 0)
+        {
+            coll.gameObject.GetComponent<Health>().Hit(_baseDamage);
+            coll.gameObject.GetComponent<KnockbackOnDamageTaken>().KnockbackPlayer(transform.position);
+            coll.gameObject.GetComponent<InvincibilityAfterBeingHit>().StartFlicker();
+
+            _damageTimer = _baseDamageTimer;
+        }
+    }
+}
