9729053d76381f81dbc9e53f6e8769aec048369f
Philippe Boutin <philboutin@hotmail.com>
2016-10-26 14:12:30 -0400

    A different music for each world is played via Polygon Collider objects MusicZone & music tracks added

---
+++ Assets/Scripts/Environment/PlayMusicZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMusicZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
