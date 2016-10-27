718d7de940864a8b31b140e78e720a8e8cdbc55b
Philippe Boutin <philboutin@hotmail.com>
2016-10-27 12:32:14 -0400

    Throwing weapons can't go inside spieks

--- Assets/Scripts/Weapon/Axe/AxeHandleHitWall.cs
+++ Assets/Scripts/Weapon/Axe/AxeHandleHitWall.cs
@@ -11,7 +11,8 @@ public class AxeHandleHitWall : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "Wall" || collider.gameObject.tag == "LevelWall" || (collider.gameObject.tag == "FlyingPlatform" && transform.parent.GetComponent<Rigidbody2D>().velocity.y < 0))
+        if (collider.gameObject.tag == "Wall" || collider.gameObject.tag == "LevelWall" || collider.gameObject.tag == "Spike"
+            || (collider.gameObject.tag == "FlyingPlatform" && transform.parent.GetComponent<Rigidbody2D>().velocity.y < 0))
         {
             GetComponent<PolygonCollider2D>().isTrigger = false;
             transform.parent.GetComponentInChildren<PolygonCollider2D>().isTrigger = false;
