f9c8abef4558de04477d646a2236f9f4cc0df1c6
Philippe Boutin <philboutin@hotmail.com>
2016-11-03 12:27:31 -0400

    Added Health item

--- Assets/Scripts/Actors/Health.cs
+++ Assets/Scripts/Actors/Health.cs
@@ -6,6 +6,7 @@ public class Health : MonoBehaviour
 {
     [SerializeField]
     private float _health = 1000f;
+    public float MaxHealth { get; private set; }
 
     public float HealthPoint { get { return _health; } set { _health = value; } }
 
@@ -20,11 +21,16 @@ public class Health : MonoBehaviour
 
     private void Start()
     {
+        MaxHealth = _health;
         OnHealthChanged += ChangeHealth;
     }
 
     public void Heal(int healPoints)
     {
+        if(_health + healPoints > MaxHealth)
+        {
+            healPoints = (int)(MaxHealth - _health);
+        }
         OnHeal(healPoints);
         OnHealthChanged(healPoints);
     }
