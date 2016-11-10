e4881aa9fbaad89408db8747e484bed497681093
Vincent Dumont <dumontv@outlook.com>
2016-11-10 13:24:51 -0500

    Added ItemCanvas to StaticObjects and removed some GameObject.Find

--- Assets/Scripts/StaticObjects.cs
+++ Assets/Scripts/StaticObjects.cs
@@ -8,6 +8,7 @@ public class StaticObjects : MonoBehaviour
     private static GameObject _panelUI;
     private static GameObject _pauseMenu;
     private static GameObject _healthBar;
+    private static GameObject _itemCanvas;
 
     private void Start()
     {
@@ -15,6 +16,7 @@ public class StaticObjects : MonoBehaviour
         _panelUI = GameObject.Find("PanelUI");
         _pauseMenu = GameObject.Find("PauseMenu");
         _healthBar = GameObject.Find("HealthBar");
+        _itemCanvas = GameObject.Find("ItemCanvas");
     }
 
     public static GameObject GetPlayer()
@@ -35,5 +37,10 @@ public class StaticObjects : MonoBehaviour
     public static GameObject GetHealthBar()
     {
         return _healthBar;
+    }
+
+    public static GameObject GetItemCanvas()
+    {
+        return _itemCanvas;
     }
 }
