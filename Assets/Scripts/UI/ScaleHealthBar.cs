6dd2943a2369e15dd469dd8b3276cbd5aa8cbe85
Vincent Dumont <dumontv@outlook.com>
2016-11-10 13:47:54 -0500

    Healthbar scaling is just right 👌

--- Assets/Scripts/UI/ScaleHealthBar.cs
+++ Assets/Scripts/UI/ScaleHealthBar.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class ScaleHealthBar : MonoBehaviour
 {
+    [SerializeField]
+    private const float FINAL_SIZE_MARGIN = 0.0002f;
+    [SerializeField]
+    private const int SCALING_SPEED = 4;
 
-    private float _initialRectangleX;
     private Transform _healthBar;
     private Health _health;
+    private Vector3 _finalSize;
+
+    private float _initialRectangleX;
 
     private void Start()
     {
@@ -19,17 +25,19 @@ public class ScaleHealthBar : MonoBehaviour
 
     private void OnHealthChanged(int hitPoints)
     {
-        StartCoroutine("ScaleHealthBarCouroutine", 
-            new Vector3(_initialRectangleX - (100 - ((_health.HealthPoint - 10) * 100) / 1000) * _initialRectangleX / 100,
-                   _healthBar.localScale.y, _healthBar.localScale.z));
+        _finalSize = new Vector3(_initialRectangleX - (100 - ((_health.HealthPoint - 10)*100)/1000)*_initialRectangleX/100,
+                                _healthBar.localScale.y, _healthBar.localScale.z);
+        StartCoroutine("ScaleHealthBarCouroutine");
     }
 
-    private IEnumerator ScaleHealthBarCouroutine(Vector3 finalSize)
+    private IEnumerator ScaleHealthBarCouroutine()
     {
-        while (_healthBar.localScale != finalSize)
+        while (_healthBar.localScale.x > _finalSize.x + FINAL_SIZE_MARGIN ||
+               _healthBar.localScale.x < _finalSize.x - FINAL_SIZE_MARGIN)
         {
-            _healthBar.localScale = Vector3.Lerp(_healthBar.localScale, finalSize, Time.fixedDeltaTime);
+            _healthBar.localScale = Vector3.Lerp(_healthBar.localScale, _finalSize, Time.deltaTime * SCALING_SPEED);
             yield return null;
         }
+        _healthBar.localScale = _finalSize;
     }
 }
