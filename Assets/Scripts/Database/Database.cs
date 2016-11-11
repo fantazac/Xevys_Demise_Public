5d44bbf621fcc075f10fbc6260770461141203c6
Philippe Boutin <philboutin@hotmail.com>
2016-11-11 10:48:07 -0500

    Can load the content of a STATS entry into memory & update inventory by the content of Database

--- Assets/Scripts/Database/Database.cs
+++ Assets/Scripts/Database/Database.cs
@@ -4,12 +4,15 @@ using Mono.Data.Sqlite;
 using System.Data;
 using System.IO;
 using System;
-using System.Runtime.Serialization.Formatters.Binary;
 
 public class Database : MonoBehaviour
 {
+    public delegate void OnInventoryReloadedHandler(bool knifeEnabled, bool axeEnabled, bool featherEnabled, bool bootsEnabled, bool bubbleEnabled, bool armorEnabled, bool earthArtefactEnabled, bool airArtefactEnabled, bool waterArtefactEnabled, bool fireArtefactEnabled);
+    public static event OnInventoryReloadedHandler OnInventoryReloaded;
+
     private IDbConnection _dbconn;
     private IDbCommand _dbcmd;
+    InventoryManager _inventoryManager;
 
     //TEMP
     private int _accountID = 0;
@@ -36,17 +39,17 @@ public class Database : MonoBehaviour
         _dbconn = (IDbConnection)new SqliteConnection(conn);
         _dbcmd = _dbconn.CreateCommand();
 
-        InventoryManager inventoryManager = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryManager>();
-        inventoryManager.OnEnableKnife += EnableKnife;
-        inventoryManager.OnEnableAxe += EnableAxe;
-        inventoryManager.OnEnableFeather += EnableFeather;
-        inventoryManager.OnEnableIronBoots += EnableBoots;
-        inventoryManager.OnEnableBubble += EnableBubble;
-        inventoryManager.OnEnableFireProofArmor += EnableArmor;
-        inventoryManager.OnEnableEarthArtefact += EnableEarthArtefact;
-        inventoryManager.OnEnableAirArtefact += EnableAirArtefact;
-        inventoryManager.OnEnableWaterArtefact += EnableWaterArtefact;
-        inventoryManager.OnEnableFireArtefact += EnableFireArtefact;
+        _inventoryManager = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryManager>();
+        _inventoryManager.OnEnableKnife += EnableKnife;
+        _inventoryManager.OnEnableAxe += EnableAxe;
+        _inventoryManager.OnEnableFeather += EnableFeather;
+        _inventoryManager.OnEnableIronBoots += EnableBoots;
+        _inventoryManager.OnEnableBubble += EnableBubble;
+        _inventoryManager.OnEnableFireProofArmor += EnableArmor;
+        _inventoryManager.OnEnableEarthArtefact += EnableEarthArtefact;
+        _inventoryManager.OnEnableAirArtefact += EnableAirArtefact;
+        _inventoryManager.OnEnableWaterArtefact += EnableWaterArtefact;
+        _inventoryManager.OnEnableFireArtefact += EnableFireArtefact;
 
         DestroyEnemyOnDeath.OnEnemyDeath += EnemyKilled;
 
@@ -84,11 +87,41 @@ public class Database : MonoBehaviour
     public void SaveStats()
     {
         _dbconn.Open();
-        string sqlQuery = String.Format("UPDATE STATS SET NB_KILLED_SCARABS = {0}, NB_KILLED_BATS = {1}, NB_KILLED_SKELTALS = {2}, KNIFE_PICKED = {3}, AXE_PICKED = {4}, FEATHER_PICKED = {5}, BOOTS_PICKED = {6}, BUBBLE_PICKED = {7}, ARMOR_PICKED = {8}, ARTEFACT1_PICKED = {9}, ARTEFACT2_PICKED = {10}, ARTEFACT3_PICKED = {11}, ARTEFACT4_PICKED = {12} WHERE ACCOUNT_ID = {13}",
-            _nbScarabsKilled, _nbBatsKilled, _nbSkeltalsKilled, _knifeEnabled, _axeEnabled, _featherEnabled, _bootsEnabled, _bubbleEnabled, _armorEnabled, _earthArtefactEnabled, _airArtefactEnabled, _waterArtefactEnabled, _fireArtefactEnabled, _accountID);
+        string sqlQuery = String.Format("UPDATE STATS SET NB_KILLED_SCARABS = {0}, NB_KILLED_BATS = {1}, NB_KILLED_SKELTALS = {2}, KNIFE_PICKED = {3}, AXE_PICKED = {4}, FEATHER_PICKED = {5}, BOOTS_PICKED = {6}, BUBBLE_PICKED = {7}, ARMOR_PICKED = {8}, ARTEFACT1_PICKED = {9}, ARTEFACT2_PICKED = {10}, ARTEFACT3_PICKED = {11}, ARTEFACT4_PICKED = {12} " +
+            "WHERE ACCOUNT_ID = {13}", _nbScarabsKilled, _nbBatsKilled, _nbSkeltalsKilled, _knifeEnabled, _axeEnabled,1, _bootsEnabled, _bubbleEnabled, _armorEnabled, _earthArtefactEnabled, _airArtefactEnabled, _waterArtefactEnabled, _fireArtefactEnabled, _accountID);
         _dbcmd.CommandText = sqlQuery;
         _dbcmd.ExecuteNonQuery();
         _dbconn.Close();
+    }
+
+    private void LoadStats()
+    {
+        _dbconn.Open();
+        string sqlQuery = String.Format("SELECT NB_KILLED_SCARABS, NB_KILLED_BATS, NB_KILLED_SKELTALS, KNIFE_PICKED, AXE_PICKED, FEATHER_PICKED, BOOTS_PICKED, BUBBLE_PICKED, ARMOR_PICKED, ARTEFACT1_PICKED, ARTEFACT2_PICKED, ARTEFACT3_PICKED, ARTEFACT4_PICKED"  +
+            " FROM STATS WHERE ACCOUNT_ID = {0}", _accountID);
+        _dbcmd.CommandText = sqlQuery;
+        IDataReader reader = _dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            _nbScarabsKilled = reader.GetInt32(0);
+            _nbBatsKilled = reader.GetInt32(1);
+            _nbSkeltalsKilled = reader.GetInt32(2);
+            _knifeEnabled = reader.GetInt32(3);
+            _axeEnabled = reader.GetInt32(4);
+            _featherEnabled = reader.GetInt32(5);
+            _bootsEnabled = reader.GetInt32(6);
+            _bubbleEnabled = reader.GetInt32(7);
+            _armorEnabled = reader.GetInt32(8);
+            _earthArtefactEnabled = reader.GetInt32(9);
+            _airArtefactEnabled = reader.GetInt32(10);
+            _waterArtefactEnabled = reader.GetInt32(11);
+            _fireArtefactEnabled = reader.GetInt32(12);
+        }
+        reader.Close();
+        reader = null;
+        _dbconn.Close();
+
+        OnInventoryReloaded(Convert.ToBoolean(_knifeEnabled), Convert.ToBoolean(_axeEnabled), Convert.ToBoolean(_featherEnabled), Convert.ToBoolean(_bootsEnabled), Convert.ToBoolean(_bubbleEnabled), Convert.ToBoolean(_armorEnabled), Convert.ToBoolean(_earthArtefactEnabled), Convert.ToBoolean(_airArtefactEnabled), Convert.ToBoolean(_waterArtefactEnabled), Convert.ToBoolean(_fireArtefactEnabled));
     }
 
     private void EnemyKilled(string tag)
