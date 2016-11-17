2d16a19fb49aaaa4c978d11798b0c326c203ce16
Vincent Dumont <dumontv@outlook.com>
2016-11-17 10:37:12 -0500

    Working on switches

---
+++ Assets/EyeState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeState : MonoBehaviour
+{
+    private Animator _anim;
+    private ActivateTrigger _activateTrigger;
+
+	private void Start()
+	{
+	    _anim = GetComponent<Animator>();
+	    _activateTrigger = GetComponentInParent<ActivateTrigger>();
+	    _activateTrigger.OnTrigger += OnTriggerActivated;
+
+	}
+
+    private void OnTriggerActivated()
+    {
+        _anim.SetBool("IsDead", true);
+    }
+}
