84a08ebd6c29b5e95fd634c4124cb83fd8d87ad3
Philippe Boutin <philboutin@hotmail.com>
2016-11-19 17:13:59 -0500

    Pause audio menu can now change music & sfx volume.

--- Assets/Scripts/Audio/AudioSettings.cs
+++ Assets/Scripts/Audio/AudioSettings.cs
@@ -1,19 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
 
 public class AudioSettings : MonoBehaviour
 {
     public delegate void OnVolumeChangedHandler(bool isMusic, float volume);
     public static event OnVolumeChangedHandler OnVolumeChanged;
 
-    public void SetMusicVolume(float volume)
+    private Slider _musicVolumeSlider;
+    private Slider _fxVolumeSlider;
+    private Switch _musicSwitch;
+
+    private float _musicVolumeBeforeDesactivate;
+
+    private void Start()
     {
+        _musicSwitch = GetComponentInChildren<Switch>();
+        Slider[] sliders = GetComponentsInChildren<Slider>();
+        _musicVolumeSlider = sliders[0];
+        _fxVolumeSlider = sliders[1];
+    }
+
+    public void SetMusicVolume(Single volume)
+    {
+        if (!_musicSwitch.isOn && _musicVolumeSlider.value > 0f)
+        {
+            _musicVolumeBeforeDesactivate = _musicVolumeSlider.value;
+            _musicSwitch.isOn = true;
+        }
         OnVolumeChanged(true, volume);
     }
 
-    public void SetSoundVolume(float volume)
+    public void SetSoundVolume(Single volume)
     {
         OnVolumeChanged(false, volume);
+    }
+
+    public void MusicState(bool activate)
+    {
+        if (activate)
+        {
+            _musicVolumeSlider.value = _musicVolumeBeforeDesactivate;
+        }
+        else
+        {
+            _musicVolumeBeforeDesactivate = _musicVolumeSlider.value;
+            _musicVolumeSlider.value = 0f;
+        }
     }
 }
