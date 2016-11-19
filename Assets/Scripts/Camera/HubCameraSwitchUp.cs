96d9616aa1ff1fe4e058cc4eed885b7eaf3af53e
Vincent Dumont <vdumont@monancai.com>
2016-11-19 14:02:56 -0500

    Fixed camera in hub

---
+++ Assets/Scripts/Camera/HubCameraSwitchUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HubCameraSwitchUp : HubCameraSwitch
+{
+    private void Start ()
+	{
+        base.Start();
+        _nbNode1 = 38;
+        _nbNode2 = 0;
+    }
+}
