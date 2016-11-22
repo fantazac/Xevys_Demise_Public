6b3ba67e5eb25ad73f3274ded968fd493d68afc6
Alexandre <alex>
2016-11-21 20:29:44 -0500

    Tags files readded

---
+++ Assets/Scripts/Tags/FindTags.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindTags : MonoBehaviour
+{
+    public string NeptuneHead { get; private set; }
+    public string Vulcan { get; private set; }
+    public string CharacterBasicAttackBox { get; private set; }
+    public string CharacterCroutchedHitbox { get; private set; }
+    public string CharacterFloatingHitbox { get; private set; }
+    public string Boundary { get; private set; }
+    public string MainCamera { get; private set; }
+    public string ResumeBtn { get; private set; }
+
+    private void Start()
+    {
+        GameObject.Find("");
+
+        NeptuneHead = "Neptune Head";
+        Vulcan = "Vulcan";
+        CharacterBasicAttackBox = "CharacterBasicAttackBox";
+        CharacterCroutchedHitbox = "CharacterCroutchedHitbox";
+        CharacterFloatingHitbox = "CharacterFloatingHitbox";
+        Boundary = "Boundary";
+        MainCamera = "MainCamera";
+        ResumeBtn = "ResumeBtn";
+    }
+
+}
