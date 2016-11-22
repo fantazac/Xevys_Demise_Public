a459b10633625de90c023081a45d2cf8af1b3cbe
Vincent Dumont <vdumont@monancai.com>
2016-11-21 21:48:46 -0500

    Tweaked falldamage

--- Assets/Scripts/Actors/Player/FallDamage.cs
+++ Assets/Scripts/Actors/Player/FallDamage.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class FallDamage: MonoBehaviour
 {
+    [SerializeField]
+    private float _timeBeforeHit = 0.75f;
+    [SerializeField]
+    private float _damageMultiplier = 100f;
+
     private Health _playerHealth;
     private PlayerMovement _playerMovement;
 
     private float _fallingCount = 0;
-
-    private const float DAMAGE_MULTIPLIER = 50f;
 
     private void Start()
     {
@@ -26,10 +29,10 @@ public class FallDamage: MonoBehaviour
 
     private void OnLanding()
     {
-        if (_fallingCount > 1 && !StaticObjects.GetPlayerState().IsInvincible && _playerHealth.HealthPoint > 0)
+        if (_fallingCount > _timeBeforeHit && !StaticObjects.GetPlayerState().IsInvincible && _playerHealth.HealthPoint > 0)
         {
-            _playerHealth.Hit((int)Mathf.Clamp(_fallingCount * DAMAGE_MULTIPLIER, 
-                _fallingCount * DAMAGE_MULTIPLIER, _playerHealth.HealthPoint), 
+            _playerHealth.Hit((int)Mathf.Clamp(_fallingCount * _damageMultiplier, 
+                _fallingCount * _damageMultiplier, _playerHealth.HealthPoint), 
                 new Vector2(transform.position.x, transform.position.y - 1));
         }
 
