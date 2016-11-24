851f7bce69f33fb134deae4559b4721e99a44936
Vincent <dumontv@outlook.com>
2016-11-24 10:36:12 -0500

    Crow in hub can now be hit

---
+++ Assets/OnAttackCrow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnAttackCrow : MonoBehaviour
+{
+    private Animator _animator;
+    private WaitForSeconds _hitAnimationDelay;
+
+    private void Start ()
+	{
+        _hitAnimationDelay = new WaitForSeconds(0.2f);
+        _animator = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "BasicAttackHitbox" || collider.tag == "Knife" || collider.tag == "Axe")
+        {
+            _animator.SetBool("IsHit", true);
+            StartCoroutine(WaitForAnimationCoroutine());
+        }
+    }
+
+    private IEnumerator WaitForAnimationCoroutine()
+    {
+        yield return _hitAnimationDelay;
+        _animator.SetBool("IsHit", false);
+    }
+
+}
