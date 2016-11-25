d4ccbb267a8628a090ec864a2ca0a2a016a79432
Alexandre <alex>
2016-11-25 07:56:33 -0500

    Improved wall outside of behemoth

---
+++ Assets/Scripts/Trigger/ActivateHitboxOfObjectsOnBreakableItemDestroyed.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateHitboxOfObjectsOnBreakableItemDestroyed : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] _hitboxesToActivate;
+
+    [SerializeField]
+    private bool _deactivateHitboxes = false;
+
+    private void Start()
+    {
+        GetComponent<Health>().OnDeath += Activate;
+    }
+
+    private void Activate()
+    {
+        foreach (GameObject hitbox in _hitboxesToActivate)
+        {
+            hitbox.GetComponent<BoxCollider2D>().enabled = !_deactivateHitboxes;
+        }
+    }
+}
