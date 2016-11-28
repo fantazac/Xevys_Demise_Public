a46593637ed36870b8fca1e76d04aeb2a2d5ee51
Alexandre <alex>
2016-11-27 22:48:19 -0500

    Working on void

---
+++ Assets/Scripts/Trigger/MoveObjectOnEvent/MoveObjectOnEntityDestroyed.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObjectOnEntityDestroyed : MoveObjectOnEvent
+{
+    [SerializeField]
+    private GameObject _entity;
+
+    protected override void Start()
+    {
+        _entity.GetComponent<Health>().OnDeath += StartObjectMovement;
+
+        base.Start();
+    }
+}
