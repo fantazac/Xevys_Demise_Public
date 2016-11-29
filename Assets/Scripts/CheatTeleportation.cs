a4fd8c56779e3df54e3edd199032d65c22e84641
Alexandre <alex>
2016-11-29 14:32:11 -0500

    Created cheats

---
+++ Assets/Scripts/CheatTeleportation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatTeleportation : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool _cheatsEnabled = false;
+
+    [SerializeField]
+    private Vector3[] _itemLocations;
+
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = StaticObjects.GetPlayer();
+        _player.GetComponentInChildren<InputManager>().OnCheat += TeleportToItem;
+    }
+
+    private void TeleportToItem(int item)
+    {
+        if (_cheatsEnabled)
+        {
+            _player.transform.position = _itemLocations[item];
+        }
+    }
+}
