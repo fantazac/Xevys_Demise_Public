dd62e4e7dc653fed208e1188e459335e02a9b277
Platou <william.cote@bell.net>
2016-11-30 14:52:03 -0500

    Working on main menu

---
+++ Assets/Scripts/Menus/MainMenu/MainMenuCurrentInterfaceAnimator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MainMenuCurrentInterfaceAnimator : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+}
--- Assets/Scripts/Menus/PauseMenu/PauseMenuSetActiveState.cs
+++ Assets/Scripts/Menus/PauseMenu/PauseMenuSetActiveState.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PauseMenuSetActiveState : MonoBehaviour
