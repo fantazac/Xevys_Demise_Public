628f1b43fcffaa0a6b9b80a0c0545126ade53874
Platou <william.cote@bell.net>
2016-12-02 09:14:02 -0500

    delete "gameSaved" string on screen + pause menu is now scale with screen size

--- Assets/Scripts/Environment/CheckpointSave.cs
+++ Assets/Scripts/Environment/CheckpointSave.cs
@@ -8,10 +8,6 @@ public class CheckpointSave : MonoBehaviour
     [SerializeField]
     private int _checkpoint = 0;
 
-    private bool _showText;
-    private int _showCounter;
-    private string _gameSaved = "Game saved";
-
     private AccountStats _accountStats;
     private SpawnToCheckpointAfterDeath _checkpointSpawn;
 
@@ -21,34 +17,12 @@ public class CheckpointSave : MonoBehaviour
         _accountStats = StaticObjects.GetDatabase().GetComponent<AccountStats>();
     }
 
-    private void Update()
-    {
-        if(_showText)
-        {
-            _showCounter--;
-            if(_showCounter <= 0)
-            {
-                _showText = false;
-            }
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == StaticObjects.GetUnityTags().Player)
         {
-            _showText = true;
-            _showCounter = _baseShowCounter;
             _accountStats.SaveTemporaryStats();
             _checkpointSpawn.SaveCheckpoint(_checkpoint);
-        }
-    }
-
-    private void OnGUI()
-    {
-        if(_showText)
-        {
-            GUI.Label(new Rect(30, 40, 100, 20), _gameSaved);
         }
     }
 }
