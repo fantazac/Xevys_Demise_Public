843798ec53224440733983f936f61dc921dce3b3
Vincent <dumontv@outlook.com>
2016-12-05 15:28:18 -0500

    Working on tooltips

---
+++ Assets/Scripts/UI/Tooltips/AttackHubTooltip.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackHubTooltip : MonoBehaviour {
+
+    private WaitForSeconds _delayBeforeFadeIn;
+    private WaitForSeconds _delayBeforeFadeOut;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _delayBeforeFadeIn = new WaitForSeconds(5);
+        _delayBeforeFadeOut = new WaitForSeconds(4);
+        StartCoroutine(FadeInFadeOutCoroutine());
+
+    }
+
+    private IEnumerator FadeInFadeOutCoroutine()
+    {
+        yield return _delayBeforeFadeIn;
+        _animator.SetTrigger("FadeIn");
+        yield return _delayBeforeFadeOut;
+        _animator.SetTrigger("FadeOut");
+    }
+}
