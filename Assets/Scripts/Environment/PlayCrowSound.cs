905adb5c68341aef6d7507e9ceea08a754a8e089
Alexandre <alex>
2016-12-07 10:10:09 -0500

    Changed crow scripts

---
+++ Assets/Scripts/Environment/PlayCrowSound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayCrowSound : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        GetComponent<AnimateCrowOnPlayerHit>().OnCrowAnimationStart += PlaySound;
+    }
+
+    private void PlaySound()
+    {
+        _audioSource.Play();
+    }
+}
