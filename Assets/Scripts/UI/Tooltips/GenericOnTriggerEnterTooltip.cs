7b2265a3044a49ecd85e8680761ee0c4ec8f4778
Vincent <dumontv@outlook.com>
2016-12-07 13:17:04 -0500

    Working on tooltips

---
+++ Assets/Scripts/UI/Tooltips/GenericOnTriggerEnterTooltip.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericOnTriggerEnterTooltip : MonoBehaviour
+{
+    private WaitForSeconds _delayBeforeFadeOut;
+    private Animator _animator;
+
+    private void Start ()
+    {
+        _animator = GetComponent<Animator>();
+        _delayBeforeFadeOut = new WaitForSeconds(2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            _animator.SetTrigger("FadeIn");
+            GetComponent<BoxCollider2D>().enabled = false;
+            StartCoroutine(FadeOutCoroutine());
+        }
+    }
+
+    private IEnumerator FadeOutCoroutine()
+    {
+        yield return _delayBeforeFadeOut;
+        _animator.SetTrigger("FadeOut");
+    }
+}
