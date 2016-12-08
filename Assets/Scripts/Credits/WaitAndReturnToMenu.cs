651b79b91fc2f9b6ea5260d056fd3258c79d1466
Platou <william.cote@bell.net>
2016-12-08 12:37:33 -0500

    Credits scene done + added to build settings

---
+++ Assets/Scripts/Credits/WaitAndReturnToMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31.TransitionKit;
+
+public class WaitAndReturnToMenu : MonoBehaviour
+{
+    public void OnScrollingEnded()
+    {
+        StartCoroutine(WaitForTwoSeconds());
+    }
+
+    private IEnumerator WaitForTwoSeconds()
+    {
+        yield return new WaitForSeconds(2f);
+
+        var fader = new FadeTransition()
+        {
+            nextScene = 1,
+            fadedDelay = 0.2f,
+            fadeToColor = Color.black
+        };
+        TransitionKit.instance.transitionWithDelegate(fader);
+    }
+
+}
