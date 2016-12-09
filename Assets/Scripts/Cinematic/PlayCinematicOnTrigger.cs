f9e43b8bfd215e07a07529660bf9237b63e5299b
Alexandre <alex>
2016-12-09 10:31:57 -0500

    Cinematic on fire artefact pickup

--- Assets/Scripts/Cinematic/PlayCinematicOnTrigger.cs
+++ Assets/Scripts/Cinematic/PlayCinematicOnTrigger.cs
@@ -12,23 +12,26 @@ public class PlayCinematicOnTrigger : MonoBehaviour
     [SerializeField]
     private float _cinematicDuration = 3f;
 
+    private PauseGame _pauseGame;
+
     private CinematicManager _cinematicManager;
 
     private void Start()
     {
+        _pauseGame = StaticObjects.GetPause().GetComponent<PauseGame>();
         _cinematicManager = StaticObjects.GetCinematic().GetComponent<CinematicManager>();
         GetComponent<ActivateTrigger>().OnTrigger += StartCinematic;
     }
 
     private void StartCinematic()
     {
+        _pauseGame.Pause();
+        _cinematicManager.CinematicIsPlaying = true;
         StartCoroutine(PlayCinematicAfterInitialDelay());
     }
 
     private IEnumerator PlayCinematicAfterInitialDelay()
     {
-        _cinematicManager.CinematicIsPlaying = true;
-        
         yield return new WaitForSecondsRealtime(_timeBeforeCinematic);
         
         _cinematicManager.StartCinematic(_cinematicPosition, _cinematicDuration);
