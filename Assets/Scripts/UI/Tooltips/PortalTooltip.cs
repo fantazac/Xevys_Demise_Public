105b5d7ea3918dc3b8d136303358a6e15cf9f092
Vincent Dumont <dumontv@outlook.com>
2016-12-10 15:22:44 -0500

    Added tooltip when FireArtefact picked up + Fixed Neptune

---
+++ Assets/Scripts/UI/Tooltips/PortalTooltip.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 
+ * SPAG ALERT : Ne pas corriger
+ * Ce sont des features de dernières minutes volontairement faites à la vite
+ * sans trop réfléchir 
+*/
+
+public class PortalTooltip : MonoBehaviour
+{
+    private Animator _animator;
+    private InventoryManager _inventoryManager;
+    private bool _hasArtefact = false;
+
+    private void Start ()
+    {
+        _animator = GetComponent<Animator>();
+        _inventoryManager = StaticObjects.GetPlayer().GetComponent<InventoryManager>();
+        _inventoryManager.OnEnableFireArtefact += OnArtefactPickedUp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player" && _hasArtefact)
+        {
+            _animator.SetTrigger("FadeIn");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Player" && _hasArtefact)
+        {
+            _animator.SetTrigger("FadeOut");
+        }
+    }
+
+    private void OnArtefactPickedUp()
+    {
+        _hasArtefact = true;
+        _animator.SetTrigger("FadeIn");
+    }	
+}
