b61e880ec14e8f179d603a2ddf686925eef5860d
William <william.cote@bell.net>
2016-12-11 20:16:55 -0500

    Finished Credits

---
+++ Assets/Scripts/Credits/MainMenuAfterAnimation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuAfterAnimation : MonoBehaviour
+{
+    public void OnAnimEnded()
+    {
+        GameObject.Find("Transition").GetComponent<ShowMainMenu>().ShowMenu();
+    }
+}
