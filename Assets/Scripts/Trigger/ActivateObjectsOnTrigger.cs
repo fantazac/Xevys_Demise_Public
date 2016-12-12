5ba5036ff5c2760cc187620a9a84d3ab83ef47b0
Vincent <dumontv@outlook.com>
2016-12-12 08:23:42 -0500

    Fixed artefact bug on reload

--- Assets/Scripts/Trigger/ActivateObjectsOnTrigger.cs
+++ Assets/Scripts/Trigger/ActivateObjectsOnTrigger.cs
@@ -14,9 +14,30 @@ public class ActivateObjectsOnTrigger : MonoBehaviour
     private void Activate()
     {
         GameObject.Find("CharacterSprite").GetComponent<Animator>().SetBool("HasSword", true);
+
         foreach (GameObject objectToActivate in _objectsToActivate)
         {
             objectToActivate.SetActive(true);
+        }
+
+        if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().EarthArtefactEnabled)
+        {
+            GameObject.Find("Green").GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().AirArtefactEnabled)
+        {
+            GameObject.Find("Yellow").GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().WaterArtefactEnabled)
+        {
+            GameObject.Find("Blue").GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().FireArtefactEnabled)
+        {
+            GameObject.Find("Red").GetComponent<SpriteRenderer>().enabled = true;
         }
     }
 }
