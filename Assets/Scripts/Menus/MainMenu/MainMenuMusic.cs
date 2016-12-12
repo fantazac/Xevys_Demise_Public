334167533040c800b02dff97f9782c76d5676641
Philippe Boutin <philboutin@hotmail.com>
2016-12-11 20:42:08 -0500

    MainMenuMusic now affected by sound parameters

---
+++ Assets/Scripts/Menus/MainMenu/MainMenuMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    private AudioSource _music;
+
+    private void Start()
+    {
+        _music = GetComponent<AudioSource>();
+        MainMenuAudioSettingsManager.OnVolumeChanged += ChangeMusicVolume;
+    }
+
+    private void OnDestroy()
+    {
+        MainMenuAudioSettingsManager.OnVolumeChanged -= ChangeMusicVolume;
+    }
+
+    private void ChangeMusicVolume(bool isMusic, float volume)
+    {
+        if (isMusic)
+        {
+            _music.volume = volume;
+        }
+    }
+}
