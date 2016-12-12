5c7641b546780ef577d259b4f5429186dddad8ac
Vincent <dumontv@outlook.com>
2016-12-12 11:44:17 -0500

    Tooltips are now disabled on reload when you already picked the item

--- Assets/Scripts/RoomState/ActivateTriggersOnRoomCleaned.cs
+++ Assets/Scripts/RoomState/ActivateTriggersOnRoomCleaned.cs
@@ -60,6 +60,49 @@ public class ActivateTriggersOnRoomCleaned : MonoBehaviour
             GameObject.Find("Xevy Spell").SetActive(false);
             GameObject.Find("Xevy Hub").SetActive(false);
             StartCoroutine(WaitForNextFrameToActivateSword());
+
+            if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().KnifeEnabled)
+            {
+                GameObject.Find("XboxKnifeTooltip").SetActive(false);
+                GameObject.Find("KeyboardKnifeTooltip").SetActive(false);
+            }
+
+            if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().AxeEnabled)
+            {
+                GameObject.Find("XboxAxeTooltip").SetActive(false);
+                GameObject.Find("KeyboardAxeTooltip").SetActive(false);
+            }
+
+            if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().KnifeEnabled &&
+                StaticObjects.GetPlayer().GetComponent<InventoryManager>().AxeEnabled)
+            {
+                GameObject.Find("XboxChangeWeaponTooltip").SetActive(false);
+                GameObject.Find("KeyboardChangeWeaponTooltip").SetActive(false);
+                GameObject.Find("XboxChangeWeaponTooltip").SetActive(false);
+                GameObject.Find("KeyboardChangeWeaponTooltip").SetActive(false);
+            }
+
+            if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().FeatherEnabled)
+            {
+                GameObject.Find("KeyboardFeatherTooltip").SetActive(false);
+                GameObject.Find("XboxFeatherTooltip").SetActive(false);
+            }
+
+            if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().IronBootsEnabled)
+            {
+                GameObject.Find("KeyboardIronBootsTooltip").SetActive(false);
+                GameObject.Find("XboxIronBootsTooltip").SetActive(false);
+            }
+
+            if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().BubbleEnabled)
+            {
+                GameObject.Find("BubbleTooltip").SetActive(false);
+            }
+
+            if (StaticObjects.GetPlayer().GetComponent<InventoryManager>().FireProofArmorEnabled)
+            {
+                GameObject.Find("FireArmorTooltip").SetActive(false);
+            }
         }
     }
 
@@ -68,5 +111,6 @@ public class ActivateTriggersOnRoomCleaned : MonoBehaviour
         yield return null;
 
         _sword.SetActive(true);
+        _sword.GetComponentInChildren<AudioSource>().enabled = false;
     }
 }
